68f08c473a461a051ec4c2acf80fd316a71f6fc5
butterdogo <olle.axelsson@elev.ga.ntig.se>
2024-02-15 10:39:44 +0100

    code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner tgb = new Scanner(System.in);
+        double konto = tgb.nextInt();
+
+        System.out.println("    Saldo    ||    år");
+        System.out.println("--------------------------");
+        for (int i = 1; i < 13 ; i++)	{
+
+            konto=konto*1.12;
+            System.out.format("%8.2f kr  ||  %3d år\n",konto,i);
+        }
+
+
+
     }
+
+
+}
+
 }
